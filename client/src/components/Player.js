e40ee12de8f883ebb044bbd730c5be0fc846c4d9
Anthony <anthony.kizer88@gmail.com>
2019-11-01 10:52:59 -0700

    added some basic styling

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Player = props => {
   return (
-    <div>
+    <div className="card">
       <h3 data-testid="player-name">{props.player.name}</h3>
       <p data-testid="player-country">Country: {props.player.country}</p>
       <p data-testid="player-searches">Searches: {props.player.searches}</p>
